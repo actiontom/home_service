6be647ff903f2a9b4352fc58491cebfb75afddfd
John Thomas Riley <action.tom@hotmail.com>
2019-11-09 11:59:17 +0200

    added access contole headers, and mongo save to greet

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 'use strict';
 
 const Express = require('./express.js');
-const Mongo = require('./mongo.js');
-
 let server = new Express();
-
 server.start();
+
+
+const AssistantV1 = require('ibm-watson/assistant/v1');
+
+const assistant = new AssistantV1({
+  version: '2019-02-28',
+  iam_apikey: 'DJQrlgxu5HZQA5-QqXjVEYpEcnvbFuW-kD-Sr0K5ob6g',
+  url: 'https://gateway-lon.watsonplatform.net/assistant/api',
+  // disable_ssl_verification: true,
+});
+
+// assistant.listWorkspaces()
+//   .then(res => {
+//     console.log(JSON.stringify(res, null, 2));
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   });
+
+// let response = assistant.message({
+//     workspace_id: 'b4d038cc-d84b-4163-a92b-4015d27ee23f',
+//     input: {'text': 'Hello'}
+//     })
+//     .then(res => {
+//       console.log(JSON.stringify(res, null, 2));
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     });
+
+    //console.log(response);
+
--- express.js
+++ express.js
@@ -8,31 +8,36 @@ constructor(){
 this.app = express();
 this.port =  3000;
 
+this.app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 }
+
+
 
 start(){
 
 this.app.use(express.json());
 
 this.app.get('/api/greet', (req, res) => {
-    
     var name = req.query.name ? req.query.name : '';
+    saveData(req.query);
     res.send('Hello ' + name);
 });
 
 this.app.get('/api/name', (req, res) => res.send('Thomas'));
 
-
 this.app.post('/api/data', (req, res) => {
 
     saveData(req.body);
-    res.send(req.body);  
+    res.send(req.body);
 
     });
 
-
 this.app.listen(this.port, () => console.log(`home_service app listening on port ${this.port}!`));
-
 }
 
 }
@@ -41,7 +46,7 @@ function saveData(obj) {
     
 let db = new Mongo();
 
-db.connect().then(()=>{   
+db.connect().then(()=>{
     db.insertOne('test', obj);
 });
 
